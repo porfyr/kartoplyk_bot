27971014b37e8a0aee317cc092ad8e05a8d64308
porfyr <rock097abc@gmail.com>
2023-02-21 00:07:14 +0200

    водка пить гулять валятся

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import openai
+import aiohttp
+import datetime
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import aiohttp
+
+def do_nothing():
+    _a = 0
+    # yes I'm pro ebat
 
 token = "<>"
 openai.api_key = "<>"
@@ -20,13 +25,16 @@ async def fetch(url, proxy=None, proxy_auth=None):
 
 
 # фільтр тексту
-def get_text(text):
+def get_text(message: types.Message):
+    text = message.text
     if text[:12] == "Картоплику, ":
         return ("--force_ua", text[12:].strip())
     elif text[:10] == "Картоплик ":
         return ("--force_ua", text[10:].strip())
     elif text[:10] == ":noprefix ":
         return ("--noprefix", text[10:].strip())
+    elif message.reply_to_message != None and message.reply_to_message.from_user.username == "kartoplyk_bot":
+        return ("--force_ua", text)
     else:
         return ("nth", "488")
 
@@ -53,7 +61,7 @@ async def echo(message: types.Message):
     # ip = await fetch(GET_IP_URL, bot.proxy, bot.proxy_auth)
     await message.answer("На связі")
     try:
-        response = get_openai_responce("hehe")
+        _response = get_openai_responce("hehe")
     except openai.error.AuthenticationError:
         await message.answer("Виплюнувся токен openai")
     except RuntimeWarning:
@@ -64,7 +72,6 @@ async def echo(message: types.Message):
         print(e)
         print("}!\n")
         _response = get_openai_responce("hehe")
-
     print("на связі))")
 
 
@@ -72,14 +79,16 @@ async def echo(message: types.Message):
 # main message handler #
 @dp.message_handler()
 async def reaction(message: types.Message):
-    (parameter, text) = get_text(message.text)
-    if text != "488" and parameter == "--force_ua":
+    (parameter, text) = get_text(message)
+    t0 = datetime.datetime.now()
+    prefix = "дай відповідь українською мовою, " if parameter == "--force_ua" else ""
+    if text != "488":
         chat_name = message.chat.title if message.chat.title != None else "пп"
-        print("\nПослано запит на openai API ...        ", message.from_id, str(message.from_user.full_name), "@"+str(message.from_user.username), "|", str(chat_name))
+        print("\nПослано "+(parameter if parameter == "--noprefix" else "")+" запит на openai API ...        ", str(chat_name), "|", datetime.datetime.now().strftime("%m.%d %H:%M:%S"))
         print("текст: "+text)
         try:
-            response = get_openai_responce( "дай відповідь українською мовою, "+text )
-            await message.answer(response['choices'][0]['text'])
+            response = get_openai_responce(prefix+text)
+            await message.reply(response['choices'][0]['text'])
         except openai.error.AuthenticationError:
             await message.answer("Сервер сплюнув токен, пишіть розрабу чи тому хто його знає щоб вставив новий")
         except RuntimeWarning:
@@ -87,29 +96,14 @@ async def reaction(message: types.Message):
         except:
             await message.answer("В такому разі хз чому не робе, будіть розраба")
         else:
-            print("текст: "+text)
-            print("Отвєт:"+response['choices'][0]['text'][1:]+"\n")
-            # print(response)
-    elif text != "488" and parameter == "--noprefix":
-        chat_name = message.chat.title if message.chat.title != None else "пп"
-        print("\nПослано --noprefix запит на openai API ...        ", message.from_id, str(message.from_user.full_name), "@"+str(message.from_user.username), "|", str(chat_name))
-        print("текст: >"+text)
-        try:
-            response = get_openai_responce( text )
-            await message.answer(response['choices'][0]['text'])
-        except openai.error.AuthenticationError:
-            await message.answer("Сервер сплюнув токен, пишіть розрабу чи тому хто його знає щоб вставив новий")
-        except RuntimeWarning:
-            await message.answer("Швидше всього перевикористано токен openai, потрібно вставити новий з іншого акаунта")
-        except:
-            await message.answer("Хз чому не роблю, будіть розраба")
-            # print(response)
-        else:
-            print("текст: "+text)
-            print("Отвєт:"+response['choices'][0]['text'][1:]+"\n")
-            # print(response)
+            d_time = round((datetime.datetime.now() - t0).total_seconds())
+            print("\n"+"текст: "+text)
+            print("Отвєт:", response['choices'][0]['text'][1:]+"\n"+"Δ", str(d_time)+"s"+"\n")
     else:
         print("не канає")
 
 
+
 executor.start_polling(dp, skip_updates=True)
+
+#Δ
