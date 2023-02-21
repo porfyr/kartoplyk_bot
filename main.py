9ca3e84973324bb26d8f36b775e2a5a7d18d43ab
porfyr <rock097abc@gmail.com>
2023-02-21 03:08:44 +0200

    водка пить гулять валятся

--- main.py
+++ main.py
@@ -30,8 +30,10 @@ def get_text(message: types.Message):
         return ("--force_ua", text[10:].strip())
     elif text[:10] == ":noprefix ":
         return ("--noprefix", text[10:].strip())
+    elif message.chat.type == "private":
+        return ("--force_ua", text.strip())
     elif message.reply_to_message != None and message.reply_to_message.from_user.username == "kartoplyk_bot":
-        return ("--force_ua", text)
+        return ("--force_ua", text.strip())
     else:
         return ("nth", "488")
 
@@ -58,17 +60,17 @@ async def echo(message: types.Message):
     # ip = await fetch(GET_IP_URL, bot.proxy, bot.proxy_auth)
     await message.answer("На связі")
     try:
-        _response = get_openai_responce("hehe")
+        _ = get_openai_responce("hehe")
     except openai.error.AuthenticationError:
         await message.answer("Виплюнувся токен openai")
     except RuntimeWarning:
-            await message.answer("Перевикористано токен openai, потрібно вставити новий з іншого акаунта")
+            await message.answer("Перевикористано токен openai, потрібно вставити новий з іншого акаунту")
     except Exception as e:
         await message.answer("Хз чому не роблю, будіть розраба")
         print("\n!{")
         print(e)
         print("}!\n")
-        _response = get_openai_responce("hehe")
+        _ = get_openai_responce("hehe")
     print("на связі))")
 
 
@@ -81,7 +83,7 @@ async def reaction(message: types.Message):
     prefix = "дай відповідь українською мовою, " if parameter == "--force_ua" else ""
     if text != "488":
         chat_name = message.chat.title if message.chat.title != None else "пп"
-        print("\nПослано "+(parameter if parameter == "--noprefix " else "")+"запит на openai API ...        ", str(chat_name), "|", datetime.datetime.now().strftime("%m.%d %H:%M:%S"))
+        print("\nПослано "+(parameter+" " if parameter == "--noprefix" else "")+"запит на openai API ...        ", str(chat_name), "|", datetime.datetime.now().strftime("%m.%d %H:%M:%S"))
         print("текст: "+text)
         try:
             response = get_openai_responce(prefix+text)
